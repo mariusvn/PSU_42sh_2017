3c62b16a349cd85c3395ca2ceaf7f8d328e00067
Luc Braun-Exposito <luc.braun-exposito@epitech.eu>
2018-05-18 15:59:15 +0200

    test

--- src/binary_tree/redirections.c
+++ src/binary_tree/redirections.c
@@ -16,11 +16,11 @@
 
 void execute_r_right(bt_node_t *node, ressources_t *rsces)
 {
-	if (is_exists(node->child[1]->sentence) == true && is_check_perm(node->child[0]->sentence, node->child[0]->sentence) == true) {
+	if (is_exists(node->child[1]->sentence) == true) {
 		node->child[0]->out = open(node->child[1]->sentence, O_RDWR);
 		execute_bt_node(node->child[0], NULL, rsces);
 		close(node->child[0]->out);
-	} else if( is_exists(node->child[1]->sentence) == false && is_check_perm(node->child[0]->sentence, node->child[0]->sentence) == true) {
+	} else if(is_exists(node->child[1]->sentence) == false) {
 		node->child[0]->out = open(node->child[1]->sentence, O_CREAT | O_RDWR, S_IRWXU);
 		execute_bt_node(node->child[0], NULL, rsces);
 		close(node->child[0]->out);
